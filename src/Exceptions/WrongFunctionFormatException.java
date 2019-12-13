e68438aa438dfade12b16d946e8580e2bab10247
AlexPasechnuy <alexpasechnuy@gmail.com>
2019-12-13 19:31:28 +0200

    Exception package created. WrongFunctionFormatException created

---
+++ src/Exceptions/WrongFunctionFormatException.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class WrongFunctionFormatException extends Exception {
+    public WrongFunctionFormatException(){
+        super("Wrong function definition");
+    }
+}
